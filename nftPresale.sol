6231f0bf52da8834a9566a0d4b519a41b5b29eca
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2023-05-04 19:02:45 +0530

    Update nftPresale.sol

--- nftPresale.sol
+++ nftPresale.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
+import "./IERC721.sol";
+import "./IERC20.sol";
+import "./ReentrancyGuard.sol";
+import "./Ownable.sol";
+import "./Address.sol";
 
 /**
 @title presale contract by d3Launch 
