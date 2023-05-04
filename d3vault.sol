a6fa2961e0a9aee62628aa53aa90c91895d6b658
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2023-05-04 19:03:23 +0530

    Update d3vault.sol

--- d3vault.sol
+++ d3vault.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
+import "./IERC20.sol";
+import "./ReentrancyGuard.sol";
+import "./Ownable.sol";
+import "./Address.sol";
 
 /**
 @title asset holder contract by d3Launch 
